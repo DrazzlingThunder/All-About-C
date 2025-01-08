20c73de940ecefe4aa713ef54934c5e2254fbee7
DrazzlingThunder <93040280+DrazzlingThunder@users.noreply.github.com>
2025-01-08 20:27:52 +0530

    3.c

---
+++ 3.c
@@ -0,0 +1,13 @@
+//To make basic operational calculator for operations on 2 digits ... (+,-,*,/,%)
+#include<stdio.h>
+int main ()
+{
+  int a,b;
+  printf("Enter two numbers");
+
+  printf("Enter first number : ");
+  scanf("%d",&a);
+  printf("Enter second number : ");
+  scanf("%d",&b);
+  // as discussed earlier 
+}
