811fb5c067674627834452cf5eee468a23e26087
Pratyush Kumar <93040280+DrazzlingThunder@users.noreply.github.com>
2025-01-09 00:04:46 +0530

    6.c

---
+++ 6.c
@@ -0,0 +1,20 @@
+/*All about operands 
+• + (Addition) 
+• - (Subtraction) 
+• * (Multiplication) 
+• / (Division) 
+• % (Modulus) */
+
+// eg-:
+
+#include<stdio.h>
+int main()
+{
+  
+int b = 2, c = 3; 
+int z; z = b*c; // We have to assign vales like this  
+//int z; b*c = z;   ... we cannot do like this (not allowed)
+  printf("Value of z is : %d",z); /// prints the value of z . 
+  return  0 ;
+  
+}
