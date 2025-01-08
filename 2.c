079c882df6aa89928df41e5e2fa9cd7d0d3e9347
DrazzlingThunder <93040280+DrazzlingThunder@users.noreply.github.com>
2025-01-08 20:24:37 +0530

    2.c

---
+++ 2.c
@@ -0,0 +1,22 @@
+// Sum of numbers in c program 
+#include<stdio.h>
+int main()
+{
+  int a,b;
+  // scanf is used to take input from user 
+  printf("Enter Two digits of which you want sum : \n");
+  printf("\n Enter First digit : ");
+  scanf("%d",&a);
+  printf("\n Enter Second digit : ");
+  scanf("%d",&b);
+  // We can take out sum in two ways 
+
+  // By introducing int data type named as sum 
+  int sum ;
+  sum=a+b;
+  printf("The sum of the digits is : %d\n",sum);
+  // By doing evaliation directly 
+  printf("The sum of the digits is : %d\n",a+b);
+  return 0;
+  
+}
