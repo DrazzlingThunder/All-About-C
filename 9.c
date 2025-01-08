22d1b249b3605aa1501925d8f5baad16dfaead30
Pratyush Kumar <93040280+DrazzlingThunder@users.noreply.github.com>
2025-01-09 00:36:26 +0530

    Switch simple program

---
+++ 9.c
@@ -0,0 +1,42 @@
+// We are going to use switch as conditional statements 
+// This program will print days of week as inputted by user ... 
+
+#include <stdio.h>
+
+int main() {
+    int week;
+
+    // Input for week
+    printf("Enter a number for the week (1-7): ");
+    scanf("%d", &week);
+
+    // Using switch to print the week
+    switch (week) {
+        case 1:
+            printf("Sunday\n");
+            break;
+        case 2:
+            printf("Monday\n");
+            break;
+        case 3:
+            printf("Tuesday\n");
+            break;
+        case 4:
+            printf("Wednesday\n");
+            break;
+        case 5:
+            printf("Thursday\n");
+            break;
+        case 6:
+            printf("Friday\n");
+            break;
+        case 7:
+            printf("Saturday\n");
+            break;
+        default:
+            printf("Invalid week number! Please enter a number between 1 and 7.\n");
+            return 1;
+    }
+
+    return 0;
+}
